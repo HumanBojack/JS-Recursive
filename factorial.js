54f687403c892e5959275ac89fd4de7d744bdb8a
HumanBojack <spychalar59@gmail.com>
2021-04-12 16:38:11 +0200

    factorial.js

--- factorial.js
+++ factorial.js
@@ -1,8 +1,8 @@
-let number = 5 //prompt("Please enter the number of which you want the Factorial");
+let number = 4 //prompt("Please enter the number of which you want the Factorial");
 
 const FactorialIterative = (n) => {
 	let accumulator = n;
-	for (var i = 1; i < n; i++) {
+	for (let i = 1; i < n; i++) {
 		accumulator *= (n - i);
 	}
 	return accumulator
