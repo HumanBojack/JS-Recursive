bfa394df3ddde658dfda48ce23288a5dc9d184fd
HumanBojack <spychalar59@gmail.com>
2021-04-12 09:55:31 +0200

    Recursive and Iterative Factorial function

---
+++ script_01.js
@@ -0,0 +1,15 @@
+let number = 5 //prompt("Please enter the number of which you want the Factorial");
+
+const FactorialIterative = (n) => {
+	let accumulator = n;
+	for (var i = 1; i < n; i++) {
+		accumulator *= (n - i);
+	}
+	return accumulator
+}
+console.log(FactorialIterative(number));
+
+const FactorialRecursive = (n) => {
+	return (n >= 1) ? (n * FactorialRecursive(n - 1)) : (1)
+}
+console.log(FactorialRecursive(number));
