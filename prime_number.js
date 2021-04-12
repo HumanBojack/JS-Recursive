de8aad156033e84fc73cdcbbfa797aea1b1ffbcd
HumanBojack <spychalar59@gmail.com>
2021-04-12 10:54:33 +0200

    better prime number checker

--- prime_number.js
+++ prime_number.js
@@ -1,11 +1,19 @@
-let isPrime = true;
-let count = 1;
-const isPrimeNumber = (n) => {
+// let isPrime = true;
+// let count = 1;
+// const isPrimeNumber = (n) => {
+// 	count++
+// 	if (n % count == 0){
+// 		isPrime = false;
+// 	}	
+// 	return (count < number) ? (isPrimeNumber(n)) : (isPrime)
+// }
+
+const isPrimeNumber = (n, isPrime = true, count = 1) => {
 	count++
 	if (n % count == 0){
 		isPrime = false;
 	}	
-	return (count < number) ? (isPrimeNumber(n)) : (isPrime)
+	return (count < number) ? (isPrimeNumber(n, isPrime, count)) : (isPrime)
 }
 
-console.log(isPrimeNumber(28));
+console.log(isPrimeNumber(29));
